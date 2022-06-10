5a5f580936866be4dd972e0c099068d96bcb0194
idanaslund <idanaslund@users.noreply.github.com>
2022-06-10 18:03:34 +0200

    dropdowns and checkboxes

--- src/pages/FilteringPage.js
+++ src/pages/FilteringPage.js
@@ -54,6 +54,170 @@ const FilteringPage = () => {
 
   return (
     <StyledRestaurantList>
+      <form>
+      <label>Type of food</label>
+      <select name="Foods">
+        <option value ="Nordic">Nordic</option>
+        <option value ="Swedish">Swedish</option>
+        <option value ="Italian">Italian</option>
+        <option value ="Asian">Asian</option>
+        <option value ="Spanish">Spanish</option>
+        <option value ="American">American</option>
+        <option value ="European">European</option>
+        <option value ="Mediterranian">Mediterranian</option>
+        <option value ="Japanese">Japanese</option>
+        <option value ="Latin American">Latin American</option>
+        <option value ="Middle Eastern">Middle Eastern</option>
+      </select>
+      </form>
+
+      <form>
+      <label>Bugdet</label>
+      <select name="Budget">
+        <option value ="Low">Low</option>
+        <option value ="Medium">Medium</option>
+        <option value ="High">High</option>
+      </select>
+      </form>
+
+      <form>
+      <label>Portion size</label>
+      <select name="Portion size">
+        <option value ="Small">Small</option>
+        <option value ="Medium">Medium</option>
+        <option value ="Large">Large</option>
+      </select>
+      </form>
+
+      <form>
+      <label>Meals</label>
+      <select name="Meals">
+        <option value ="Breakfast">Breakfast</option>
+        <option value ="Brunch">Brunch</option>
+        <option value ="Lunch">Lunch</option>
+        <option value ="Dinner">Dinner</option>
+      </select>
+      </form>
+
+      <form>
+      <label>Target audience</label>
+      <select name="Target audience">
+        <option value ="Group">Group</option>
+        <option value ="Family">Family</option>
+        <option value ="Date">Date</option>
+      </select>
+      </form>
+
+      <form>
+      <label>Restaurant focus</label>
+      <select name="Restaurant focus">
+        <option value ="Vegan">Vegan</option>
+        <option value ="Vegetarian">Vegetarian</option>
+        <option value ="Fish">Fish</option>
+        <option value ="Meat">Meat</option>
+      </select>
+      </form>
+      
+      <form>Dogfriendly
+        <label>Yes
+        <input type="radio" value="true"/>
+        </label>
+        <label>No
+        <input type="radio" value="false"/>
+        </label>
+      </form>
+
+      <form>Outdoor area
+        <label>Yes
+        <input type="radio" value="true"/>
+        </label>
+        <label>No
+        <input type="radio" value="false"/>
+        </label>
+      </form>
+
+
+      <form>Budget
+        <label>Low
+        <input type="checkbox" value="Low"/>
+        </label>
+        <label>Medium
+        <input type="checkbox" value="Medium"/>
+        </label>
+        <label>High
+        <input type="checkbox" value="High"/>
+        </label>
+        <label>Low
+        <input type="checkbox" value="Low"/>
+        </label>
+        <label>Medium
+        <input type="checkbox" value="Medium"/>
+        </label>
+        <label>High
+        <input type="checkbox" value="High"/>
+        </label>
+      </form>
+
+      <form>Budget
+        <label>Low
+        <input type="checkbox" value="Low"/>
+        </label>
+        <label>Medium
+        <input type="checkbox" value="Medium"/>
+        </label>
+        <label>High
+        <input type="checkbox" value="High"/>
+        </label>
+        <label>Low
+        <input type="checkbox" value="Low"/>
+        </label>
+        <label>Medium
+        <input type="checkbox" value="Medium"/>
+        </label>
+        <label>High
+        <input type="checkbox" value="High"/>
+        </label>
+      </form>
+
+      <form>Budget
+        <label>Low
+        <input type="checkbox" value="Low"/>
+        </label>
+        <label>Medium
+        <input type="checkbox" value="Medium"/>
+        </label>
+        <label>High
+        <input type="checkbox" value="High"/>
+        </label>
+        <label>Low
+        <input type="checkbox" value="Low"/>
+        </label>
+        <label>Medium
+        <input type="checkbox" value="Medium"/>
+        </label>
+        <label>High
+        <input type="checkbox" value="High"/>
+        </label>
+        <label>Low
+        <input type="checkbox" value="Low"/>
+        </label>
+        <label>Medium
+        <input type="checkbox" value="Medium"/>
+        </label>
+        <label>High
+        <input type="checkbox" value="High"/>
+        </label>
+        <label>Low
+        <input type="checkbox" value="Low"/>
+        </label>
+        <label>Medium
+        <input type="checkbox" value="Medium"/>
+        </label>
+        <label>High
+        <input type="checkbox" value="High"/>
+        </label>
+      </form>
+     
       <div className='restaurantListPage'>
         {restaurants.map(restaurant => (
           <Link key={restaurant.id} state={{restaurantId: restaurant.id}} to={`/restaurants/${restaurant.id}`}>
