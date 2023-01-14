80f1cdb97d238f7153022c68f81978e41a72eb44
idanaslund <idanaslund@users.noreply.github.com>
2023-01-14 16:26:45 +0100

    changed url for backend, heroku now on ida

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,8 +1,10 @@
-export const BASE_URL = 'https://restaurants-backend-database.herokuapp.com'
+export const BASE_URL = 'https://new-restaurants-backend.herokuapp.com/'
 
 export const SIGNUP = 'signup'
 
-export const EDIT_USER = (id) => `https://restaurants-backend-database.herokuapp.com/profile/${id}`
+export const EDIT_USER = (id) => `https://new-restaurants-backend.herokuapp.com/profile/${id}`
+
+// `https://restaurants-backend-database.herokuapp.com/profile/${id}`
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`
 
